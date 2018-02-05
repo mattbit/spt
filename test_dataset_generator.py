601e1b88d6a13b682185074133e625625a8e46d2
mattbit <me@mattbit.com>
2018-02-05 02:57:37 +0100

    Test dataset

---
+++ test_dataset_generator.py
@@ -0,0 +1,48 @@
+import csv
+import locale
+import numpy as np
+
+# Set to users preferred locale:
+locale.setlocale(locale.LC_ALL, '')
+
+N = 10000
+TIMESTEP = 0.125
+N_STEPS = 100
+
+def drift(x, y):
+    return 0, 0
+
+def diffusion(x, y):
+    return 1, 1
+
+
+# %%
+# Run for N_STEPS steps
+xx = np.zeros((N_STEPS, N))
+yy = np.zeros_like(xx)
+
+# Initial positions
+xx[0] = np.random.random(N) * 60
+yy[0] = np.random.random(N) * 60
+
+for i in range(1, N_STEPS):
+    drift_x, drift_y = drift(xx[i-1], yy[i-1])
+    diff = diffusion(xx[i-1], yy[i-1])[0]
+    
+    xx[i] = xx[i-1] + drift_x*TIMESTEP + diff*np.random.normal(size=N)
+    yy[i] = yy[i-1] + drift_y*TIMESTEP + diff*np.random.normal(size=N)
+
+# Create CSV
+
+with open("test_dataset.csv", "w", newline="") as output:
+    writer = csv.writer(output, delimiter=";")
+
+    # Header
+    writer.writerow(["track id", "t", "x", "y"])
+
+    for n in range(N):
+        for t in range(N_STEPS):
+            x = locale.format("%f", xx[t, n])
+            y = locale.format("%f", yy[t, n])
+
+            writer.writerow([n, t, x, y])
